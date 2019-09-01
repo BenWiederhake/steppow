55a40e837ca275621b7d9f0faa957af3f9b1063c
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2019-09-01 04:13:05 +0200

    Implement verification
    Oh boy was that easy.

--- src/verify.py
+++ src/verify.py
@@ -4,70 +4,63 @@ from hashlib import sha256
 from math import ceil, log2
 
 TEST_CERTS = [
-    bytes(4),
-    bytes(1400),
+    (b'', 7, 7, 800, bytes(10), -1, False),
+    (b'', 7, 7, 800, bytes(1400), -1, False),
+    (b'', 7, 7, 33, b'\x04\x64\x02\xA0\x13\x80\x47\x03\xF4\x04\x60\x1A\x01\x33\x0C\xAC\x04\x00\x12\x40\x4E\x01\x6C\x0B\x90\x52\x40\x85\x02\x1C\x0C\x20\x35\x01\x3A\x05\xF4\x0A\x90\x3A\x00\x9E\x00\xE0\x14\xA0\x24\x40\xEB\x01\xE4\x0B\xA0\x38\x80\x68\x06\x2C', 6596, True),
+    (b'x', 7, 7, 33, b'\x04\x64\x02\xA0\x13\x80\x47\x03\xF4\x04\x60\x1A\x01\x33\x0C\xAC\x04\x00\x12\x40\x4E\x01\x6C\x0B\x90\x52\x40\x85\x02\x1C\x0C\x20\x35\x01\x3A\x05\xF4\x0A\x90\x3A\x00\x9E\x00\xE0\x14\xA0\x24\x40\xEB\x01\xE4\x0B\xA0\x38\x80\x68\x06\x2C', -1, False),
+    (b'', 7, 7, 33, b'\x04\x64\x02\xA0\x13\x80\x47\x03\xF4\x04\x60\x1A\x01\x33\x0C\xAC\x04\x00\x12\x40\x4E\x01\x6C\x0B\x90\x52\x41\x85\x02\x1C\x0C\x20\x35\x01\x3A\x05\xF4\x0A\x90\x3A\x00\x9E\x00\xE0\x14\xA0\x24\x40\xEB\x01\xE4\x0B\xA0\x38\x80\x68\x06\x2C', -1, False),
 ]
 
 
-def read_params(filename):
-    with open(filename, 'r') as fp:
-        lines = fp.read().split('\n')
-    params = dict()
-    for l in lines:
-        if not l.startswith('#define'):
-            continue
-        if not l.startswith('#define SPOW_'):
-            print('WARNING: problems with this line:\n\t' + l)
-            continue
-        name_value = l[len('#define SPOW_'):].split(' ')
-        if len(name_value) != 2:
-            print('WARNING: cannot split line:\n\t' + l)
-            continue
-        name, value = name_value
-        name = name.lower()
-        if name in params:
-            print('Error: {} defined multiple times?!'.format(name))
-            return None
-        if value.startswith('"'):
-            value = 'b' + value
-        value = eval(value)  # TODO: Do this without eval, please?
-        params[name] = value
-    return params
-
-
-def analyze_params(prefix, difficulty, safety, steps):
+def analyze_params(prefix, difficulty, safety, steps, hashes_actual):
     print('Prefix: {}'.format(prefix))
     print('Difficulty: {}'.format(difficulty))
     print('Safety: {}'.format(safety))
     print('Steps: {}'.format(steps))
     print('--------------------')
-    print('Bits per step: {}'.format(difficulty + steps))
+    print('Bits per step: {}'.format(difficulty + safety))
     print('Probability of impossibility: < 2^({})'.format(log2(steps) - 2 ** safety))
     print('E[num hashes]: {}'.format(steps * (2 ** difficulty)))
-    print('Certificate byte length: {}'.format(ceil(steps * (difficulty + safety) / 8)))
+    print('Actual num hashes: {}'.format(hashes_actual))
+    print('Suffix byte length: {}'.format(ceil(steps * (difficulty + safety) / 8)))
+    print('--------------------')
 
 
-def try_verify(cert, prefix, difficulty, safety, steps):
-    if len(cert) != ceil(steps * (difficulty + safety) / 8):
+def check_difficulty(buf, difficulty):
+    digest = sha256(buf).digest()
+    return all(digest[i // 8] & (1 << (7 - i % 8)) == 0 for i in range(difficulty))
+
+
+def try_verify(prefix, difficulty, safety, steps, suffix):
+    if len(suffix) != ceil(steps * (difficulty + safety) / 8):
         print('Bad length.')
         return False
-    print('Checking rest: NOTIMPLEMENTED')
+    for i in range(1, steps + 1):
+        suffix_bits = (difficulty + safety) * i
+        buf = bytearray(prefix) + suffix[:(suffix_bits + 7) // 8]
+        clear_bits = (8 - suffix_bits % 8) % 8
+        buf[-1] &= ~((1 << clear_bits) - 1)
+        if not check_difficulty(buf, difficulty):
+            print('Failed at step {}, with buf {}'.format(i, buf))
+            return False
     return True
 
 
-def run_on(filename):
-    params = read_params(filename)
-    if params is None:
-        exit(1)
-    # del params['NOTNEEDED']
-    analyze_params(**params)
+def run_on(prefix, difficulty, safety, steps, suffix, hashes_actual, valid_expect):
+    analyze_params(prefix, difficulty, safety, steps, hashes_actual)
+    valid_actual = try_verify(prefix, difficulty, safety, steps, suffix)
+    print('Valid: {}'.format(valid_actual))
+    print('Matches expectation: {}'.format(valid_actual == valid_expect))
+    if valid_actual != valid_expect:
+        print('== ERROR: Wrong! ==')
+
+
+def run_all(certificates):
     for cert_i, cert in enumerate(TEST_CERTS):
-        print('--------------------')
-        print('Checking cert #{} ({}...)'.format(cert_i, cert[:80]))
-        print(try_verify(cert, **params))
+        print('========================================')
+        print('Checking cert #{}'.format(cert_i))
+        run_on(*cert)
 
 
 if __name__ == '__main__':
-    import os.path
-    # "os.path" allows calling this script from anywhere.
-    run_on(os.path.join(os.path.dirname(__file__), 'config.h'))
+    run_all(TEST_CERTS)
